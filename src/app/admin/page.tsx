38230e8100f54ab93bb9b29e86c5153f614857ab
pseudo0244 <142434147+pseudo0244@users.noreply.github.com>
2025-06-28 19:37:35 +0530

    Analytics

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -62,7 +62,7 @@ const Dashboard = () => {
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="mb-6">
             <h2 className="text-2xl font-bold text-gray-900 mb-2">Welcome Tanishq!</h2>
-            <p className="text-gray-600">Here's your website dashboard performance overview</p>
+            <p className="text-gray-600">Here&apos;s your website dashboard performance overview</p>
           </div>
 
           <Card className="shadow-lg">
